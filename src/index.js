0b94d054df7d7fc5a8f29c3317be36d93bbcc0a6
Your Name <you@example.com>
2017-12-13 14:15:21 -0200

    some changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Router, Route } from 'react-router';
+
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
